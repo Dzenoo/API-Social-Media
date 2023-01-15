9704df4e5b2774d1646c5bb7dfd6c3ec4aeb43cc
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-15 20:53:49 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,15 +6,12 @@ module.exports = (req, res, next) => {
   if (req.method === "OPTIONS") {
     return next();
   }
-
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
       throw new Error("Auth failed");
     }
-
     const decodedToken = jwt.verify(token, "");
-
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
