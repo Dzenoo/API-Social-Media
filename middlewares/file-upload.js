382e958a3ce74c4d2f4c6834af72708eb76c2a4d
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-01-15 20:54:05 +0100

    Update file-upload.js

--- middlewares/file-upload.js
+++ middlewares/file-upload.js
@@ -13,18 +13,17 @@ const fileUpload = multer({
     destination: (req, file, cb) => {
       cb(null, "uploads/images");
     },
-
     filename: (req, file, cb) => {
       const ext = MIME_TYPE[file.mimetype];
       cb(null, uuid() + "." + ext);
     },
   }),
-
   fileFilter: (req, file, cb) => {
     const isValid = !!MIME_TYPE[file.mimetype];
     let error = isValid ? null : new Error("False mime type");
     cb(error, isValid);
   },
 });
+
 
 module.exports = fileUpload;
