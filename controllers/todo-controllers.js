fafc5f83e8dddee5d7fc60308df4336ec3da65f0
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-02-16 15:58:15 +0100

    Update todo-controllers.js

--- controllers/todo-controllers.js
+++ controllers/todo-controllers.js
@@ -1,11 +1,7 @@
 const { validationResult } = require("express-validator");
-
 const mongoose = require("mongoose");
-
 const fs = require("fs");
-
 const HttpError = require("../models/http-error");
-
 const User = require("../models/user");
 const Todo = require("../models/todo");
 
