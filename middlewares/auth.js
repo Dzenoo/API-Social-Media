06b9d19e0eb5d53882c66a002862a5c96356891b
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-03-25 22:40:10 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,7 +16,6 @@ module.exports = (req, res, next) => {
       req.userData = { userId: decodedToken.userId };
       next();
   } catch (err) {
-    
     const error = new HttpError("Authentication failed!", 401);
     return next(error);
   }
