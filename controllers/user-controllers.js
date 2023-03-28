1ab48bd1cc2a8c366d032a6117b3f1d126238af5
Dzenis Gudzevic <110186379+Dzenoo@users.noreply.github.com>
2023-03-28 21:51:10 +0200

    Update user-controllers.js

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -3,7 +3,6 @@ const HttpError = require("../models/http-error");
 
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-
 const User = require("../models/user");
 
 exports.getUsers = async (req, res, next) => {
